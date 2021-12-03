4d2ae241428b114264532e3d8243693a60ea9b28
Jaspuneet Sanghera <79317392+Jaspuneet-Sanghera@users.noreply.github.com>
2021-12-03 09:41:15 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var endDate = new Date("Nov 23, 2021 12:00:00").getTime();
+
+
+var endDate = new Date("Dec 5, 2021 12:00:00").getTime();
+//Put value of image in bgImageElement
+var bgImageElement = document.getElementById('bg-img');
 
 var timer = setInterval(function() {
 
@@ -13,21 +17,30 @@ var timer = setInterval(function() {
         let secs = Math.floor((t % (1000 * 60)) / 1000);
     
         document.getElementById("timer-days").innerHTML = days +
-        "<span class='label'>DAY(S)</span>";
+        "<span class='label'>Day(s)</span>";
     
         document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) +
-        "<span class='label'>HR(S)</span>";
+        "<span class='label'>Hr(s)</span>";
     
         document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) +
-        "<span class='label'>MIN(S)</span>";
+        "<span class='label'>Min(s)</span>";
     
         document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2) +
-        "<span class='label'>SEC(S)</span>";
+            "<span class='label'>Sec(s)</span>";
+        if (secs % 2 === 0) {
+            var backimg = 'image1.png';
+
+        }
+        else {
+            var backimg = 'image2.jpg';
+        }
+        bgImageElement.src = backimg;
     
     } else {
 
         document.getElementById("timer").innerHTML = "Yay....The countdown is over!";
     
     }
+   
     
 }, 1000);
